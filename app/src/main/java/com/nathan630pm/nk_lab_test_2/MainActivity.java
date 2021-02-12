95c6fa6df997821fb62e730c6e235f11d0efbcd0
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 18:37:03 -0500

    initial commit

---
+++ app/src/main/java/com/nathan630pm/nk_lab_test_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nathan630pm.nk_lab_test_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
