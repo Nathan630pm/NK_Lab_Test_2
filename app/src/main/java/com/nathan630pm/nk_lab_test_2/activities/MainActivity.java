4249c2545d8d73f62a21ea850b74bdebb48f025d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 18:41:18 -0500

    setup app structure

--- app/src/main/java/com/nathan630pm/nk_lab_test_2/activities/MainActivity.java
+++ app/src/main/java/com/nathan630pm/nk_lab_test_2/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.nathan630pm.nk_lab_test_2;
+package com.nathan630pm.nk_lab_test_2.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.nathan630pm.nk_lab_test_2.R;
 
 public class MainActivity extends AppCompatActivity {
 
